5979406c353314a7bde0c9fd085f698d98d1e75f
Andre Wlodkovski <andremacwlod@gmail.com>
2024-05-25 11:34:47 -0300

    Fix NFT and increase compute unit limit

--- tests/mint-nft.ts
+++ tests/mint-nft.ts
@@ -4,6 +4,7 @@ import { MintNft } from "../target/types/mint_nft";
 import { MPL_TOKEN_METADATA_PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata';
 import { associatedAddress } from "@coral-xyz/anchor/dist/cjs/utils/token";
 import { getLocalAccount } from "./util";
+import { ComputeBudgetProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
 
 describe("mint-nft", () => {
   // Configure the client to use the local cluster.
@@ -14,7 +15,7 @@ describe("mint-nft", () => {
   const program = anchor.workspace.MintNft as Program<MintNft>;
   const nftTitle = 'Andre Wlod';
   const nftSymbol = 'WLOD';
-  const nftUri = 'https://media.licdn.com/dms/image/C4D03AQHqrn4tBjRtyA/profile-displayphoto-shrink_800_800/0/1600261790603?e=1721865600&v=beta&t=cG4vbDZLQgjN8iLMNJvEADrqYuBvnJqtwGn3k2zm37o';
+  const nftUri = 'https://raw.githubusercontent.com/andrewlod/solana-anchor-nft-practice/main/assets/metadata.json';
 
   const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(MPL_TOKEN_METADATA_PROGRAM_ID.toString());
 
@@ -49,7 +50,15 @@ describe("mint-nft", () => {
     );
     console.log(`Master Edition address: ${masterEditionAddress}`);
 
-    await program.methods.mint(
+    const modifyComputeUnits = ComputeBudgetProgram.setComputeUnitLimit({
+      units: 400000
+    });
+
+    const addPriorityFee = ComputeBudgetProgram.setComputeUnitPrice({
+      microLamports: 1
+    });
+
+    let mintInstruction = await program.methods.mint(
       nftTitle, nftSymbol, nftUri
     )
       .accounts({
@@ -61,7 +70,13 @@ describe("mint-nft", () => {
         tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID
       })
       .signers([mintKeypair, walletKeypair])
-      .rpc()
-      .catch(err => console.log(err));
+      .instruction();
+
+    let tx = new Transaction();
+    tx.add(modifyComputeUnits);
+    tx.add(addPriorityFee);
+    tx.add(mintInstruction);
+
+    await sendAndConfirmTransaction(provider.connection, tx, [walletKeypair, mintKeypair]);
   });
 });
